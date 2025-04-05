0bb6f7f1ff3977fa2c22ca168827fcce6e2c9edc
Eswc11 <151701840+Eswc11@users.noreply.github.com>
2025-04-05 12:42:04 +0600

    Delete user.py

--- user.py
+++
@@ -1,6 +0,0 @@
-user = {}
-user["name"] = input("What is the user's name?: ")
-user["age"] = input("What is the user's age?: ")
-user["country"] = input("What is the user's country of birth?: ")
-user["known_for"] = input("What is the user's known for?: ")
-print(user)
